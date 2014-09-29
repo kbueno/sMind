30f72d93db444cab487f47196425ac5f12050aa5
Kelsey Bueno <kbueno@localhost.localdomain>
2014-09-29 14:37:22 -0700

    V1 complete, no error checking though

--- js/network.js
+++ js/network.js
@@ -2,14 +2,20 @@ var nodes = new vis.DataSet();
 var edges = new vis.DataSet();
 var network = null;
 
-/*
-  function draw() {
+function draw(data) {
   // create a network
   var container = document.getElementById('mynetwork');
-  var data = {
-	nodes: nodes,
-	edges: edges
-  };
+
+  if (data == undefined) {
+    data = {
+	  nodes: nodes,
+	  edges: edges
+    };
+  } else {
+	nodes.add(data.nodes),
+	edges.add(data.edges)
+  }
+
   var options = {
 	stabilize: false,
 	physics: {
@@ -23,15 +29,15 @@ var network = null;
 	  dynamic:false,
 	  type: "continuous"
 	},
-
+        
 	dataManipulation: true,
 	onAdd: function(data,callback) {
 	  var span = document.getElementById('operation');
 	  var idInput = document.getElementById('node-id');
 	  var labelInput = document.getElementById('node-label');
-	  var saveButton = document.getElementById('saveButton');
-	  var cancelButton = document.getElementById('cancelButton');
-	  var div = document.getElementById('network-popUp');
+	  var saveButton = document.getElementById('saveEditButton');
+	  var cancelButton = document.getElementById('cancelEditButton');
+	  var div = document.getElementById('edit-popUp');
 	  span.innerHTML = "Add Node";
 	  idInput.value = data.id;
 	  labelInput.value = data.label;
@@ -43,9 +49,9 @@ var network = null;
 	  var span = document.getElementById('operation');
 	  var idInput = document.getElementById('node-id');
 	  var labelInput = document.getElementById('node-label');
-	  var saveButton = document.getElementById('saveButton');
-	  var cancelButton = document.getElementById('cancelButton');
-	  var div = document.getElementById('network-popUp');
+	  var saveButton = document.getElementById('saveEditButton');
+	  var cancelButton = document.getElementById('cancelEditButton');
+	  var div = document.getElementById('edit-popUp');
 	  span.innerHTML = "Edit Node";
 	  idInput.value = data.id;
 	  labelInput.value = data.label;
@@ -65,158 +71,256 @@ var network = null;
 	  }
 	}
   };
+
+  // Create the network and override the toolbar function
   network = new vis.Network(container, data, options);
+  network._createManipulatorBar = _createManipulatorBar.bind(network)
+  network._createManipulatorBar()
 
   // add event listeners
   network.on('select', function(params) {
 	document.getElementById('selection').innerHTML = 'Selection: ' + params.nodes;
   });
 
-  network.on("resize", function(params) {console.log(params.width,params.height)});
+  function clearPopUp() {
+	var saveButton = document.getElementById('saveEditButton');
+	var cancelButton = document.getElementById('cancelEditButton');
+	saveButton.onclick = null;
+	cancelButton.onclick = null;
+	var div = document.getElementById('edit-popUp');
+	div.style.display = 'none';
 
-};
-*/
-
-function onLoad() {
-  // create a network
-  var container = document.getElementById('mynetwork');
- 
-  nodes.add([
-    {id: 1, label: 'Node 1'},
-    {id: 2, label: 'Node 2'},
-    {id: 3, label: 'Node 3'},
-    {id: 4, label: 'Node 4'},
-    {id: 5, label: 'Node 5'}
-  ]);
-  edges.add([
-    {from: 1, to: 2},
-    {from: 1, to: 3},
-    {from: 2, to: 4},
-    {from: 2, to: 5}
-  ]);
- 
-  var data = {
-	nodes: nodes,
-	edges: edges
-  };
-
-  var options = {
-	stabilize: false,
-	physics: {
-	  barnesHut: {
-		gravitationalConstant:0,
-		springConstant:0,
-		centralGravity:0
-	  }
-	},
-	smoothCurves: {
-	  dynamic:false,
-	  type: "continuous"
-	}
   }
 
-  network = new vis.Network(container, data, options);
+  function saveData(data,callback) {
+	var idInput = document.getElementById('node-id');
+	var labelInput = document.getElementById('node-label');
 
-  // Test to save the map
-  saveMap();
+	data.id = idInput.value;
+	data.label = labelInput.value;
+	clearPopUp();
+	callback(data);
+
+  }
+};
+
+
+
+
+
+
+
+
+
+
+
+/* MAIN TOOLBAR */
+_createManipulatorBar = function() {
+  // remove bound functions
+  if (network.boundFunction) {
+    network.off('select', network.boundFunction);
+  }
+
+  if (network.edgeBeingEdited !== undefined) {
+    network.edgeBeingEdited._disableControlNodes();
+    network.edgeBeingEdited = undefined;
+    network.selectedControlNode = null;
+    network.controlNodesActive = false;
+  }
+
+  // restore overloaded functions
+  network._restoreOverloadedFunctions();
+
+  // resume calculation
+  network.freezeSimulation = false;
+
+  // reset global variables
+  network.blockConnectingEdgeSelection = false;
+  network.forceAppendSelection = false;
+  var toolbar = document.getElementById("network-manipulationDiv");
+  var editModeDiv = document.getElementById("network-manipulation-editMode");
+  toolbar.style.display="block";
+  editModeDiv.style.display="none";
+
+  if (network.editMode == true) {
+    while (network.manipulationDiv.hasChildNodes()) {
+      network.manipulationDiv.removeChild(network.manipulationDiv.firstChild);
+    }
+	
+	// Edit options Button
+    network.manipulationDiv.innerHTML = "" +
+      "<span class='network-manipulationUI save' id='network-manipulate-saveMap' title='Save'></span>" +
+
+      "<div class='network-seperatorLine'></div>" +
+      "<span class='network-manipulationUI load' id='network-manipulate-loadMap' title='Load'></span>" +
+      
+	  "<div class='network-seperatorLine'></div>" +
+      "<span class='network-manipulationUI add' id='network-manipulate-addNode' title='Add Node'></span>" +
+      
+	  "<div class='network-seperatorLine'></div>" +
+      "<span class='network-manipulationUI connect' id='network-manipulate-connectNode' title='Connect Node'></span>";
+    
+	if (network._getSelectedNodeCount() == 1 && network.triggerFunctions.edit) {
+      network.manipulationDiv.innerHTML += "" +
+        "<div class='network-seperatorLine'></div>" +
+        "<span class='network-manipulationUI edit' id='network-manipulate-editNode' title='Edit Node'></span>";
+    }
+    else if (network._getSelectedEdgeCount() == 1 && network._getSelectedNodeCount() == 0) {
+      network.manipulationDiv.innerHTML += "" +
+        "<div class='network-seperatorLine'></div>" +
+        "<span class='network-manipulationUI edit' id='network-manipulate-editEdge' title='Edit Edge'></span>";
+    }
+    if (network._selectionIsEmpty() == false) {
+      network.manipulationDiv.innerHTML += "" +
+        "<div class='network-seperatorLine'></div>" +
+        "<span class='network-manipulationUI delete' id='network-manipulate-delete' title='Delete Node'></span>";
+    }
+
+
+    // bind the icons
+	var saveMapButton = document.getElementById("network-manipulate-saveMap");
+	saveMapButton.onclick = onSave.bind(this);
+	var loadMapButton = document.getElementById("network-manipulate-loadMap");
+	loadMapButton.onclick = onLoad.bind(this);
+    var addNodeButton = document.getElementById("network-manipulate-addNode");
+    addNodeButton.onclick = addNodeToolbar.bind(this);
+    var addEdgeButton = document.getElementById("network-manipulate-connectNode");
+    addEdgeButton.onclick = network._createAddEdgeToolbar.bind(network);
+    
+	if (network._getSelectedNodeCount() == 1 && network.triggerFunctions.edit) {
+      var editButton = document.getElementById("network-manipulate-editNode");
+      editButton.onclick = network._editNode.bind(network);
+    }
+    else if (network._getSelectedEdgeCount() == 1 && network._getSelectedNodeCount() == 0) {
+      var editButton = document.getElementById("network-manipulate-editEdge");
+      editButton.onclick = network._createEditEdgeToolbar.bind(network);
+    }
+    if (network._selectionIsEmpty() == false) {
+      var deleteButton = document.getElementById("network-manipulate-delete");
+      deleteButton.onclick = network._deleteSelected.bind(network);
+    }
+    var closeDiv = document.getElementById("network-manipulation-closeDiv");
+    closeDiv.onclick = network._toggleEditMode.bind(network);
+
+    network.boundFunction = network._createManipulatorBar.bind(network);
+    network.on('select', network.boundFunction);
+
+  }
+  else {
+    network.manipulationDiv.innerHTML = "" +
+      "<span class='network-manipulationUI save' id='network-manipulate-saveMap' title='Save'></span>" +
+
+      "<div class='network-seperatorLine'></div>" +
+      "<span class='network-manipulationUI load' id='network-manipulate-loadMap' title='Load'></span>" +
+
+      "<div class='network-seperatorLine'></div>" +
+      "<span class='network-manipulationUI edit' id='network-manipulate-editModeButton' title='Edit'></span>";
+	
+	var saveMapButton = document.getElementById("network-manipulate-saveMap");
+	saveMapButton.onclick = onSave.bind(this);
+	var loadMapButton = document.getElementById("network-manipulate-loadMap");
+	loadMapButton.onclick = onLoad.bind(this);
+    var editModeButton = document.getElementById("network-manipulate-editModeButton");
+    editModeButton.onclick = network._toggleEditMode.bind(network);
+  }
+};
+
+
+
+
+
+function addNodeToolbar() {
+  // clear the toolbar
+  this._clearManipulatorBar();
+  if (this.boundFunction) {
+    this.off('select', this.boundFunction);
+  }
+
+  // create the toolbar contents
+  this.manipulationDiv.innerHTML = "" +
+    "<span class='network-manipulationUI back' id='network-manipulate-back'></span>" +
+    "<div class='network-seperatorLine'></div>" +
+    "<span class='network-manipulationUI none' id='network-manipulate-back'>" +
+    "<span id='network-manipulatorLabel' class='network-manipulationLabel'>" + this.constants.labels['addDescription'] + "</span></span>";
+
+  // bind the icon
+  var backButton = document.getElementById("network-manipulate-back");
+  backButton.onclick = this._createManipulatorBar.bind(this);
+
+  // we use the boundFunction so we can reference it when we unbind it from the "select" event.
+  this.boundFunction = this._addNode.bind(this);
+  this.on('select', this.boundFunction);
+};
+
+/*
+function onAdd(data,callback) {
+  this.manipulationDiv.innerHTML = "" +
+  "<input id='node-id' value='new value'>" +
+  "<input id='node-label' value='new value'>" +
+  "<input type='button' value='save' id='saveEditButton'></button>"
+	  
+  var saveButton = document.getElementById("");
+  idInput.value = data.id;
+  labelInput.value = data.label;
+  saveButton.onclick = saveData.bind(this,data,callback);
+}
+*/
+
+
+
+
+
+/* BUTTON FUNCTIONS */
+
+function onSave() {
+  var span = document.getElementById('mapOperation');
+  var idInput = document.getElementById('map-id');
+  var saveButton = document.getElementById('saveMapButton');
+  var cancelButton = document.getElementById('cancelMapButton');
+  var div = document.getElementById('map-popUp');
+  
+  span.innerHTML = "Save Map";
+  
+  $(saveButton).on('click', function(){
+    saveMap(idInput.value)
+  })
+  $(saveButton).on('click', function() {
+	clearMapPopUp()
+  })
+
+  cancelButton.onclick = clearMapPopUp.bind();
+  div.style.display = 'block';
 
 }
 
-/*
-function clearPopUp() {
-  var saveButton = document.getElementById('saveButton');
-  var cancelButton = document.getElementById('cancelButton');
+function clearMapPopUp() {
+  var saveButton = document.getElementById('saveMapButton');
+  var cancelButton = document.getElementById('cancelMapButton');
   saveButton.onclick = null;
   cancelButton.onclick = null;
-  var div = document.getElementById('network-popUp');
+  var div = document.getElementById('map-popUp');
   div.style.display = 'none';
 }
-
-function saveData(data) {
-  var idInput = document.getElementById('node-id');
-  var labelInput = document.getElementById('node-label');;
-  data.id = idInput.value;
-
-  var div = document.getElementById('network-popUp');;
-  data.id = idInput.value;
-
-  data.id = idInput.value;
-  data.label = labelInput.value;
-  clearPopUp();
-  saveMap
-}
-*/
-
-
-
-
-
-
-
-// No Selection Needed
-/*
-  function addNode() {
-  if (network._selectionIsEmpty() && network.editMode == true) {
-	var nodeCount = Object.keys(network.nodes).length + 1;
-    var positionObject = network._pointerToPositionObject(network.pointerPosition);
-    var defaultData = {
-	  id:nodeCount,
-	  x:positionObject.left,
-	  y:positionObject.top,
-	  label:nodeCount,
-	  allowedToMoveX:true,
-	  allowedToMoveY:true
-	};
-    network.nodesData.add(defaultData);
-    network._createManipulatorBar();
-    network.moving = true;
-    network.start();  
-  }
-};
-
-// Selection Needed
-function deleteNode() {
-  if (!network._selectionIsEmpty() && network.editMode == true) {
-    if (!network._clusterInSelection()) {
-      var selectedNodes = network.getSelectedNodes();
-      var selectedEdges = network.getSelectedEdges();
-      var data = {nodes: selectedNodes, edges: selectedEdges};
-	  network.edgesData.remove(selectedEdges);
-	  network.nodesData.remove(selectedNodes);
-      network._unselectAll();
-      network.moving = true;
-      network.start();
-     }
-    else {
-      alert(this.constants.labels["deleteClusterError"]);
-    }
-  }
-};
-
-function editNode() {
-
-};
-
-function connectNode() {
-
-};
-*/
-
 
 // convenience method to stringify a JSON object
 function toJSON (obj) {
   return JSON.stringify(obj, null, 4);
 };
 
-function saveMap() {
+// Save map to the server
+function saveMap(val) {
+  console.log(val)
   // Get the lists of node and edge objects
   var data = {
-	nodes: nodes.get(),
+	nodes: nodes.get({fields: ['id','label','x','y']}),
 	edges: edges.get()
   };
-  var jsonData = JSON.stringify(data, undefined, 2);
+  var jsonData = toJSON({
+	  name: val,
+	  data: data
+  });
   
-  // Send to client		
+  // Send to server		
   $.ajax({
 	type: "POST",
 	url: "data",
@@ -225,28 +329,51 @@ function saveMap() {
 	data: jsonData,
 	//on success
   });
-
-  
 };
 
-function loadMap() {
+function onLoad() {
+  var span = document.getElementById('mapOperation');
+  var idInput = document.getElementById('map-id');
+  var saveButton = document.getElementById('saveMapButton');
+  var cancelButton = document.getElementById('cancelMapButton');
+  var div = document.getElementById('map-popUp');
+  
+  span.innerHTML = "Load Map";
+  
+  $(saveButton).on('click', function() {
+	loadMap(idInput.value, draw)
+  })
+  $(saveButton).on('click', function() {
+	clearMapPopUp()
+  })
+
+  cancelButton.onclick = clearMapPopUp.bind();
+  div.style.display = 'block';
+}
+
+// Load a map from a JSON file
+function loadMap(val, callBack) {
+  // Clear the map (needed if currently working on a map)
+  if (nodes.get() != []) {
+	clearMap(); 
+  }
   // Send to server		
   $.ajax({
 	type: "GET",
-	url: "data",
+	url: "data/" + val,
 	contentType: "application/json",
 	dataType: "json",
 	//on success
 	success: function (data) {
-		nodes = data.nodes,
-		edges = data.edges
+		callBack(data);
 	}
   });
-
-
 };
 
-function newMap() {
-	nodes.clear();
-	edges.clear();
+function clearMap() {
+  alert("Are you sure you want to delete this map?");
+	
+  nodes.clear();
+  edges.clear();
 };
+
--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var app = express();
 
+var fs = require('fs');
 var bodyParser = require('body-parser');
 
 app.use(bodyParser.json());
@@ -8,30 +9,46 @@ app.use('/libs', express.static(__dirname + '/libs'));
 app.use('/css', express.static(__dirname + '/css'));
 app.use('/js', express.static(__dirname + '/js'));
 
+// Try to load any saved mind map
+var rawData = fs.readFileSync("data.json", {encoding: "utf8"});
+var data = {};
+try {
+	data = JSON.parse(rawData);
+} catch (e) {
+	data = {};
+}
+
 // This serves index.html
 app.get('/', function(req, res) {
   res.sendFile(__dirname + '/index.html');	
 });
 
-var data = {};
 // Get data from the client
 app.post('/data', function(req, res) {
-	console.log("This is working, cool");
 	console.log(req.body);
-	var id = Date.time() + Math.random() * 1000;
+	// Create a unique ID and store in data
+	var id = req.body["name"]
+	data[id] = req.body["data"];
 
-	data[id] = res.body;
+	// Send response with ID
 	res.send(200,{"id": id});
-})
+});
 
 // Send data to the client
 app.get('/data/:id', function(req, res) {
+	// Check if file exists
 	if (data[req.params.id] != undefined) {
-		res.send(200, data[req.param.id]);
+		res.send(200, data[req.params.id]);
 	} else {
-		res.send(404)
+		res.send(404);
 	}
 });
 
+// When process ends, save data to a file
+process.on("SIGINT", function () {
+	console.log(data);
+	fs.writeFileSync("data.json", JSON.stringify(data, null, 4), {encoding: "utf8"});
+	process.exit();
+});
 
 app.listen(8080)
