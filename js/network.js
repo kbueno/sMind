e688d927d42cc197df62e33e49437ebb470880a7
Kelsey Bueno <kbueno@localhost.localdomain>
2014-09-30 15:58:29 -0700

    added search bar

--- js/network.js
+++ js/network.js
@@ -4,7 +4,7 @@ var network = null;
 
 function draw(data) {
   // create a network
-  var container = document.getElementById('mynetwork');
+  var container = document.getElementById('network');
 
   if (data == undefined) {
     data = {
@@ -31,33 +31,16 @@ function draw(data) {
 	},
         
 	dataManipulation: true,
-	onAdd: function(data,callback) {
-	  var span = document.getElementById('operation');
-	  var idInput = document.getElementById('node-id');
-	  var labelInput = document.getElementById('node-label');
-	  var saveButton = document.getElementById('saveEditButton');
-	  var cancelButton = document.getElementById('cancelEditButton');
-	  var div = document.getElementById('edit-popUp');
-	  span.innerHTML = "Add Node";
-	  idInput.value = data.id;
-	  labelInput.value = data.label;
-	  saveButton.onclick = saveData.bind(this,data,callback);
-	  cancelButton.onclick = clearPopUp.bind();
-	  div.style.display = 'block';
+	onAdd: function(data, callback) {
+	  var idInput = document.getElementById('new-node-id');
+	  var labelInput = document.getElementById('new-node-label');
+	  
+	  data.id = idInput.value;
+	  data.label = labelInput.value;
+	  callback(data);
 	},
 	onEdit: function(data,callback) {
-	  var span = document.getElementById('operation');
-	  var idInput = document.getElementById('node-id');
-	  var labelInput = document.getElementById('node-label');
-	  var saveButton = document.getElementById('saveEditButton');
-	  var cancelButton = document.getElementById('cancelEditButton');
-	  var div = document.getElementById('edit-popUp');
-	  span.innerHTML = "Edit Node";
-	  idInput.value = data.id;
-	  labelInput.value = data.label;
-	  saveButton.onclick = saveData.bind(this,data,callback);
-	  cancelButton.onclick = clearPopUp.bind();
-	  div.style.display = 'block';
+		onEdit(data,callback);	
 	},
 	onConnect: function(data,callback) {
 	  if (data.from == data.to) {
@@ -82,39 +65,49 @@ function draw(data) {
 	document.getElementById('selection').innerHTML = 'Selection: ' + params.nodes;
   });
 
-  function clearPopUp() {
-	var saveButton = document.getElementById('saveEditButton');
-	var cancelButton = document.getElementById('cancelEditButton');
-	saveButton.onclick = null;
-	cancelButton.onclick = null;
-	var div = document.getElementById('edit-popUp');
-	div.style.display = 'none';
+  var submitButtom = document.getElementById('submitButton');
+  $(submitButton).on('click', function(e) {
+	e.preventDefault();
+	var input = $('#searchForm').serialize();
+	$.ajax({
+	  url: "http://localhost:8983/solr/collection1/select",
+	  jsonp: "json.wrf",
+	  dataType: "jsonp",
+	  data: input,
+	  success: function(response) {
+		displayResults(response);
+	  }
+	})
+  })
 
-  }
 
-  function saveData(data,callback) {
-	var idInput = document.getElementById('node-id');
-	var labelInput = document.getElementById('node-label');
-
-	data.id = idInput.value;
-	data.label = labelInput.value;
-	clearPopUp();
-	callback(data);
-
-  }
 };
 
 
+/*********************************************************************/
+/*                        SEARCH FUNCTIONS                           */
+/*********************************************************************/
 
 
+function displayResults(result) {
+	var data = result.response;
+	var html = "<ul>"
+	console.log(data.docs)
+	for(var i = 0; i < data.docs.length; i++) {
+		var entry = data.docs[i]
+		html += "<li>"+entry.id.substring(10)+"</li>"
+	}
+	html += "</ul>"
+	
+	document.getElementById('searchResult').innerHTML = html;
+}
 
 
+/*********************************************************************/
+/*                        MAIN TOOLBAR                               */
+/*********************************************************************/
 
 
-
-
-
-/* MAIN TOOLBAR */
 _createManipulatorBar = function() {
   // remove bound functions
   if (network.boundFunction) {
@@ -137,6 +130,7 @@ _createManipulatorBar = function() {
   // reset global variables
   network.blockConnectingEdgeSelection = false;
   network.forceAppendSelection = false;
+  
   var toolbar = document.getElementById("network-manipulationDiv");
   var editModeDiv = document.getElementById("network-manipulation-editMode");
   toolbar.style.display="block";
@@ -150,13 +144,12 @@ _createManipulatorBar = function() {
 	// Edit options Button
     network.manipulationDiv.innerHTML = "" +
       "<span class='network-manipulationUI save' id='network-manipulate-saveMap' title='Save'></span>" +
-
       "<div class='network-seperatorLine'></div>" +
       "<span class='network-manipulationUI load' id='network-manipulate-loadMap' title='Load'></span>" +
-      
+	  "<div class='network-seperatorLine'></div>" +
+      "<span class='network-manipulationUI zoomFit' id='network-manipulate-zoomFit' title='Zoom Fit'></span>" +
 	  "<div class='network-seperatorLine'></div>" +
       "<span class='network-manipulationUI add' id='network-manipulate-addNode' title='Add Node'></span>" +
-      
 	  "<div class='network-seperatorLine'></div>" +
       "<span class='network-manipulationUI connect' id='network-manipulate-connectNode' title='Connect Node'></span>";
     
@@ -176,72 +169,80 @@ _createManipulatorBar = function() {
         "<span class='network-manipulationUI delete' id='network-manipulate-delete' title='Delete Node'></span>";
     }
 
-
     // bind the icons
 	var saveMapButton = document.getElementById("network-manipulate-saveMap");
 	saveMapButton.onclick = onSave.bind(this);
 	var loadMapButton = document.getElementById("network-manipulate-loadMap");
 	loadMapButton.onclick = onLoad.bind(this);
+	var zoomFitButton = document.getElementById("network-manipulate-zoomFit");
+	zoomFitButton.onclick = onZoomFit.bind(this);
     var addNodeButton = document.getElementById("network-manipulate-addNode");
-    addNodeButton.onclick = addNodeToolbar.bind(this);
+    addNodeButton.onclick = onAdd.bind(this);
     var addEdgeButton = document.getElementById("network-manipulate-connectNode");
-    addEdgeButton.onclick = network._createAddEdgeToolbar.bind(network);
+    addEdgeButton.onclick = onConnect.bind(this);
     
 	if (network._getSelectedNodeCount() == 1 && network.triggerFunctions.edit) {
       var editButton = document.getElementById("network-manipulate-editNode");
-      editButton.onclick = network._editNode.bind(network);
+      editButton.onclick = network._editNode.bind(this);
     }
     else if (network._getSelectedEdgeCount() == 1 && network._getSelectedNodeCount() == 0) {
       var editButton = document.getElementById("network-manipulate-editEdge");
-      editButton.onclick = network._createEditEdgeToolbar.bind(network);
+      editButton.onclick = network._createEditEdgeToolbar.bind(this);
     }
     if (network._selectionIsEmpty() == false) {
       var deleteButton = document.getElementById("network-manipulate-delete");
-      deleteButton.onclick = network._deleteSelected.bind(network);
+      deleteButton.onclick = network._deleteSelected.bind(this);
     }
     var closeDiv = document.getElementById("network-manipulation-closeDiv");
-    closeDiv.onclick = network._toggleEditMode.bind(network);
+    closeDiv.onclick = network._toggleEditMode.bind(this);
 
-    network.boundFunction = network._createManipulatorBar.bind(network);
+    network.boundFunction = network._createManipulatorBar.bind(this);
     network.on('select', network.boundFunction);
 
   }
   else {
     network.manipulationDiv.innerHTML = "" +
       "<span class='network-manipulationUI save' id='network-manipulate-saveMap' title='Save'></span>" +
-
       "<div class='network-seperatorLine'></div>" +
       "<span class='network-manipulationUI load' id='network-manipulate-loadMap' title='Load'></span>" +
-
       "<div class='network-seperatorLine'></div>" +
+      "<span class='network-manipulationUI zoomFit' id='network-manipulate-zoomFit' title='Zoom Fit'></span>" +
+	  "<div class='network-seperatorLine'></div>" +
       "<span class='network-manipulationUI edit' id='network-manipulate-editModeButton' title='Edit'></span>";
 	
 	var saveMapButton = document.getElementById("network-manipulate-saveMap");
 	saveMapButton.onclick = onSave.bind(this);
 	var loadMapButton = document.getElementById("network-manipulate-loadMap");
 	loadMapButton.onclick = onLoad.bind(this);
+	var zoomFitButton = document.getElementById("network-manipulate-zoomFit");
+	zoomFitButton.onclick = onZoomFit.bind(this);
     var editModeButton = document.getElementById("network-manipulate-editModeButton");
-    editModeButton.onclick = network._toggleEditMode.bind(network);
+    editModeButton.onclick = network._toggleEditMode.bind(this);
   }
 };
 
 
+/*********************************************************************/
+/*                      BUTTON SPECIFIC TOOLBARS                     */
+/*********************************************************************/
 
 
-
-function addNodeToolbar() {
-  // clear the toolbar
+function onAdd() {
   this._clearManipulatorBar();
   if (this.boundFunction) {
     this.off('select', this.boundFunction);
   }
 
-  // create the toolbar contents
   this.manipulationDiv.innerHTML = "" +
-    "<span class='network-manipulationUI back' id='network-manipulate-back'></span>" +
+    "<span class='network-manipulationUI back' id='network-manipulate-back' title='Back'></span>" +
     "<div class='network-seperatorLine'></div>" +
     "<span class='network-manipulationUI none' id='network-manipulate-back'>" +
-    "<span id='network-manipulatorLabel' class='network-manipulationLabel'>" + this.constants.labels['addDescription'] + "</span></span>";
+    "<span id='network-manipulatorLabel' class='network-manipulationLabel'>" + 
+    "Name: </span><input id='new-node-id' value='1'>" +
+    "<span id='network-manipulatorLabel' class='network-manipulationLabel'>" + 
+    "Label: </span><input id='new-node-label' value='1'>" +
+    "<span id='network-manipulatorLabel' class='network-manipulationLabel'>" + 
+    this.constants.labels['addDescription'] + "</span></span>";
 
   // bind the icon
   var backButton = document.getElementById("network-manipulate-back");
@@ -252,55 +253,128 @@ function addNodeToolbar() {
   this.on('select', this.boundFunction);
 };
 
-/*
-function onAdd(data,callback) {
-  this.manipulationDiv.innerHTML = "" +
-  "<input id='node-id' value='new value'>" +
-  "<input id='node-label' value='new value'>" +
-  "<input type='button' value='save' id='saveEditButton'></button>"
-	  
-  var saveButton = document.getElementById("");
-  idInput.value = data.id;
-  labelInput.value = data.label;
-  saveButton.onclick = saveData.bind(this,data,callback);
-}
-*/
-
-
-
-
-
-/* BUTTON FUNCTIONS */
-
 function onSave() {
-  var span = document.getElementById('mapOperation');
-  var idInput = document.getElementById('map-id');
-  var saveButton = document.getElementById('saveMapButton');
-  var cancelButton = document.getElementById('cancelMapButton');
-  var div = document.getElementById('map-popUp');
-  
-  span.innerHTML = "Save Map";
-  
+  this.manipulationDiv.innerHTML = "" +
+    "<span class='network-manipulationUI back' id='network-manipulate-back' title='Back'></span>" +
+    "<div class='network-seperatorLine'></div>" +
+    "<span class='network-manipulationUI none' id='network-manipulate-back'>" +
+    "<input id='map-id' value='newMap'>" +
+    "<input type='button' value='save' id='save-map-button'></button></span>";
+	  
+  var backButton = document.getElementById("network-manipulate-back");
+  backButton.onclick = this._createManipulatorBar.bind(this);
+
+  var saveButton = document.getElementById("save-map-button");
+  var idInput = document.getElementById("map-id");
   $(saveButton).on('click', function(){
     saveMap(idInput.value)
   })
-  $(saveButton).on('click', function() {
-	clearMapPopUp()
+  $(saveButton).on('click', function(){
+    _createManipulatorBar();
   })
-
-  cancelButton.onclick = clearMapPopUp.bind();
-  div.style.display = 'block';
-
 }
 
-function clearMapPopUp() {
-  var saveButton = document.getElementById('saveMapButton');
-  var cancelButton = document.getElementById('cancelMapButton');
-  saveButton.onclick = null;
-  cancelButton.onclick = null;
-  var div = document.getElementById('map-popUp');
-  div.style.display = 'none';
+
+function onLoad() {
+  this.manipulationDiv.innerHTML = "" +
+    "<span class='network-manipulationUI back' id='network-manipulate-back' title='Back'></span>" +
+    "<div class='network-seperatorLine'></div>" +
+    "<span id='network-manipulatorLabel' class='network-manipulationLabel'>" + 
+    "<input id='map-id' value='mindMap1'>" +
+    "<input type='button' value='load' id='load-map-button'></button></span>";
+	  
+  var backButton = document.getElementById("network-manipulate-back");
+  backButton.onclick = this._createManipulatorBar.bind(this);
+
+  var loadButton = document.getElementById("load-map-button");
+  var idInput = document.getElementById("map-id");
+  $(loadButton).on('click', function() {
+	loadMap(idInput.value, draw)
+  })
+  $(loadButton).on('click', function() {
+	_createManipulatorBar();
+  })
 }
+
+function onZoomFit() {
+	network.zoomExtent();	
+}
+
+function onEdit(data,callback) {
+  network._clearManipulatorBar();
+  if (network.boundFunction) {
+	network.off('select', this.boundFunction);
+  }
+
+  network.manipulationDiv.innerHTML = "" +
+	"<span class='network-manipulationUI back' id='network-manipulate-back' title='Back'></span>" +
+	"<div class='network-seperatorLine'></div>" +
+	"<span class='network-manipulationUI none' id='network-manipulate-back'>" +
+	"<span id='network-manipulatorLabel' class='network-manipulationLabel'>" + 
+	"Name: </span><input id='edit-node-id' value="+data.id+">" +
+	"<span id='network-manipulatorLabel' class='network-manipulationLabel'>" + 
+	"Label: </span><input id='edit-node-label' value="+data.label+">" +
+	"<input type='button' value='save' id='edit-node-button'></button></span>";
+
+  // bind the icon
+  var backButton = document.getElementById("network-manipulate-back");
+  backButton.onclick = this._createManipulatorBar.bind(this);
+
+  var saveButton = document.getElementById("edit-node-button");
+  $(saveButton).on('click', function() {
+	  var idInput = document.getElementById('edit-node-id');
+	  var labelInput = document.getElementById('edit-node-label');
+	  
+	  data.id = idInput.value;
+	  data.label = labelInput.value;
+	  callback(data);
+  });
+}
+
+function onConnect() {
+  // clear the toolbar
+  this._clearManipulatorBar();
+  this._unselectAll(true);
+  this.freezeSimulation = true;
+
+  if (this.boundFunction) {
+    this.off('select', this.boundFunction);
+  }
+
+  this._unselectAll();
+  this.forceAppendSelection = false;
+  this.blockConnectingEdgeSelection = true;
+
+  this.manipulationDiv.innerHTML = "" +
+    "<span class='network-manipulationUI back' id='network-manipulate-back' title='Back'></span>" +
+    "<div class='network-seperatorLine'></div>" +
+    "<span class='network-manipulationUI none' id='network-manipulate-back'>" +
+    "<span id='network-manipulatorLabel' class='network-manipulationLabel'>" + 
+	this.constants.labels['linkDescription'] + "</span></span>";
+
+  // bind the icon
+  var backButton = document.getElementById("network-manipulate-back");
+  backButton.onclick = this._createManipulatorBar.bind(this);
+
+  // we use the boundFunction so we can reference it when we unbind it from the "select" event.
+  this.boundFunction = this._handleConnect.bind(this);
+  this.on('select', this.boundFunction);
+
+  // temporarily overload functions
+  this.cachedFunctions["_handleTouch"] = this._handleTouch;
+  this.cachedFunctions["_handleOnRelease"] = this._handleOnRelease;
+  this._handleTouch = this._handleConnect;
+  this._handleOnRelease = this._finishConnect;
+
+  // redraw to show the unselect
+  this._redraw();
+};
+
+
+/*********************************************************************/
+/*                        BUTTION FUNCTONS                           */
+/*********************************************************************/
+
 
 // convenience method to stringify a JSON object
 function toJSON (obj) {
@@ -312,7 +386,7 @@ function saveMap(val) {
   console.log(val)
   // Get the lists of node and edge objects
   var data = {
-	nodes: nodes.get({fields: ['id','label','x','y']}),
+	nodes: nodes.get({fields: ['id','label','x','y','allowedToMoveX','allowedToMoveY']}),
 	edges: edges.get()
   };
   var jsonData = toJSON({
@@ -331,25 +405,6 @@ function saveMap(val) {
   });
 };
 
-function onLoad() {
-  var span = document.getElementById('mapOperation');
-  var idInput = document.getElementById('map-id');
-  var saveButton = document.getElementById('saveMapButton');
-  var cancelButton = document.getElementById('cancelMapButton');
-  var div = document.getElementById('map-popUp');
-  
-  span.innerHTML = "Load Map";
-  
-  $(saveButton).on('click', function() {
-	loadMap(idInput.value, draw)
-  })
-  $(saveButton).on('click', function() {
-	clearMapPopUp()
-  })
-
-  cancelButton.onclick = clearMapPopUp.bind();
-  div.style.display = 'block';
-}
 
 // Load a map from a JSON file
 function loadMap(val, callBack) {
